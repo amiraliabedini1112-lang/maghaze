7e78d5894740ef82468655115766b92dcf2bf884
maghaze <amiraliabedini1112@gmail.com>
2026-08-16 16:26:22 +0330

    app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-```javascript
 function login() {
-    alert("دکمه ورود کار می‌کند!");
+const username = document.getElementById("username").value.trim();
+const password = document.getElementById("password").value;
+const error = document.getElementById("error");
+
+```
+if (username === "amirali" && password === "sstttat") {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("app").style.display = "block";
+    error.textContent = "";
+} else {
+    error.textContent = "نام کاربری یا رمز عبور اشتباه است.";
+}
+```
+
 }
 
 function logout() {
-    alert("خروج");
+document.getElementById("app").style.display = "none";
+document.getElementById("login").style.display = "block";
 }
-```
